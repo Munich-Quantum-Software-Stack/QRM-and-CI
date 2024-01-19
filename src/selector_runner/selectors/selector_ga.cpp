2ecee4ae392e34f7e358863bfe3def323558d4a6
Jorge Echavarria <jorge.echavarria@lrz.de>
2024-01-19 11:02:19 +0000

    Fixed path to passes

--- src/selector_runner/selectors/selector_ga.cpp
+++ src/selector_runner/selectors/selector_ga.cpp
@@ -22,48 +22,72 @@ using llvm::orc::ThreadSafeModule;
  */
 extern "C" std::vector<std::string> selector(ThreadSafeModule &TSM)
 {
+    char *passesEnv = std::getenv("PASSES");
+
+    if (passesEnv == nullptr)
+    {
+        std::cout << "   [Selector]............Warning: Environment variable "
+                     "$PASSES not found"
+                  << std::endl;
+
+        return std::vector<std::string>{};
+    }
+
+    std::string passesPath = passesEnv;
+
+    if (passesPath.find(':') != std::string::npos)
+    {
+        std::cout << "   [Selector]............Warning: Environment variable "
+                     "$PASSES contains more than one path"
+                  << std::endl;
+
+        return std::vector<std::string>{};
+    }
+
     // Append the existing passes
     std::vector<std::string> passes{
-        "libQirDivisionByZeroPass.so",
-        "libQirNormalizeArgAnglePass.so",
-        //"libQirXCnotXReductionPass.so", TODO BUG?
-        "libQirCommuteCnotRxPass.so",
-        "libQirCommuteRxCnotPass.so",
-        "libQirCommuteCnotXPass.so",
-        "libQirCommuteXCnotPass.so",
-        "libQirCommuteCnotZPass.so",
-        "libQirCommuteZCnotPass.so",
-        "libQirPlaceIrreversibleGatesInMetadataPass.so",
-        "libQirAnnotateUnsupportedGatesPass.so",
-        "libQirU3ToRzRyRzDecompositionPass.so",
-        "libQirRzToRxRyRxDecompositionPass.so",
-        "libQirCNotToHCZHDecompositionPass.so",
-        "libQirSwapToCnotsDecompositionPass.so",
-        "libQirCZToHCnotHDecompositionPass.so",
-        "libQirFunctionAnnotatorPass.so",
-        "libQirRedundantGatesCancellationPass.so",
-        "libQirFunctionReplacementPass.so",
-        "libQirReplaceConstantBranchesPass.so",
-        "libQirGroupingPass.so",
-        "libQirRemoveNonEntrypointFunctionsPass.so",
-        "libQirDeferMeasurementPass.so",
-        "libQirBarrierBeforeFinalMeasurementsPass.so",
-        "libQirRemoveBasicBlocksWithSingleNonConditionalBranchInstsPass.so",
-        //"libQirQubitRemapPass.so", TODO BREAKS THE DEPTH'S FITNESS EVALUATION
-        "libQirResourceAnnotationPass.so",
-        "libQirNullRotationCancellationPass.so",
-        "libQirMergeRotationsPass.so",
-        "libQirDoubleCnotCancellationPass.so",
-        "libQirHadamardAndXGateSwitchPass.so",
-        "libQirHadamardAndYGateSwitchPass.so",
-        "libQirHadamardAndZGateSwitchPass.so",
-        "libQirXGateAndHadamardSwitchPass.so",
-        "libQirYGateAndHadamardSwitchPass.so",
-        "libQirZGateAndHadamardSwitchPass.so",
-        "libQirSToSDaggerPass.so",
-        "libQirSDaggerToSPass.so",
-        "libQirReverseCnotPass.so",
-        "libQirSwapAndCnotReplacementPass.so",
+        passesPath + "/libQirDivisionByZeroPass.so",
+        passesPath + "/libQirNormalizeArgAnglePass.so",
+        // passesPath + "/libQirXCnotXReductionPass.so", TODO BUG?
+        passesPath + "/libQirCommuteCnotRxPass.so",
+        passesPath + "/libQirCommuteRxCnotPass.so",
+        passesPath + "/libQirCommuteCnotXPass.so",
+        passesPath + "/libQirCommuteXCnotPass.so",
+        passesPath + "/libQirCommuteCnotZPass.so",
+        passesPath + "/libQirCommuteZCnotPass.so",
+        passesPath + "/libQirPlaceIrreversibleGatesInMetadataPass.so",
+        passesPath + "/libQirAnnotateUnsupportedGatesPass.so",
+        passesPath + "/libQirU3ToRzRyRzDecompositionPass.so",
+        passesPath + "/libQirRzToRxRyRxDecompositionPass.so",
+        passesPath + "/libQirCNotToHCZHDecompositionPass.so",
+        passesPath + "/libQirSwapToCnotsDecompositionPass.so",
+        passesPath + "/libQirCZToHCnotHDecompositionPass.so",
+        passesPath + "/libQirFunctionAnnotatorPass.so",
+        passesPath + "/libQirRedundantGatesCancellationPass.so",
+        passesPath + "/libQirFunctionReplacementPass.so",
+        passesPath + "/libQirReplaceConstantBranchesPass.so",
+        passesPath + "/libQirGroupingPass.so",
+        passesPath + "/libQirRemoveNonEntrypointFunctionsPass.so",
+        passesPath + "/libQirDeferMeasurementPass.so",
+        passesPath + "/libQirBarrierBeforeFinalMeasurementsPass.so",
+        passesPath + "/libQirRemoveBasicBlocksWithSingleNonConditionalBranchIns"
+                     "tsPass.so",
+        // passesPath + "/libQirQubitRemapPass.so", TODO BREAKS THE DEPTH'S
+        // FITNESS EVALUATION
+        passesPath + "/libQirResourceAnnotationPass.so",
+        passesPath + "/libQirNullRotationCancellationPass.so",
+        passesPath + "/libQirMergeRotationsPass.so",
+        passesPath + "/libQirDoubleCnotCancellationPass.so",
+        passesPath + "/libQirHadamardAndXGateSwitchPass.so",
+        passesPath + "/libQirHadamardAndYGateSwitchPass.so",
+        passesPath + "/libQirHadamardAndZGateSwitchPass.so",
+        passesPath + "/libQirXGateAndHadamardSwitchPass.so",
+        passesPath + "/libQirYGateAndHadamardSwitchPass.so",
+        passesPath + "/libQirZGateAndHadamardSwitchPass.so",
+        passesPath + "/libQirSToSDaggerPass.so",
+        passesPath + "/libQirSDaggerToSPass.so",
+        passesPath + "/libQirReverseCnotPass.so",
+        passesPath + "/libQirSwapAndCnotReplacementPass.so",
     };
 
     // Perform the DSE
