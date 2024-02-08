b9c21069b38222455cf97c9c27a27953f348b666
Jorge Echavarria <jorge.echavarria@lrz.de>
2024-02-08 14:24:30 +0000

    Fixed bug in kill_daemons.sh script

--- scripts/kill_daemons.sh
+++ scripts/kill_daemons.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find PIDs of all daemons running
-pids=$(pgrep -f 'qresourcemanager_d' | grep -v "$(pgrep -f 'vim')")
+pids=$(pgrep -f 'qresourcemanager_d' || grep -v "$(pgrep -f 'vim')")
 
 # Check if any matching processes were found
 if [ -n "$pids" ]; then
