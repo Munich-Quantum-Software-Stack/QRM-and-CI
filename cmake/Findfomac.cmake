98db00c1b33fd3986ea457e6b1751754c1073ece
martin <martin.knudsen@wmi.badw.de>
2024-02-23 10:59:42 +0100

    set fomac branch wmi-backend in Findfomac.cmake

--- cmake/Findfomac.cmake
+++ cmake/Findfomac.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     fomac
     GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/FoMaC.git
-    GIT_TAG develop
+    GIT_TAG wmi-backend
 )
 
 FetchContent_MakeAvailable(fomac)
