280e6215cae0e63a4dab62410d4e10c3aa237460
Martin Letras <di35hec@badwlrz-cl14642>
2025-03-17 10:35:49 +0000

    🚀 Including firs running version of the pass manager

---
+++ include/mqss/QuantumResourceManager/PassRunner.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file GeneratorRunner.hpp
+ * @brief TODO
+ */
+
+#pragma once
+
+#include "mlir/IR/BuiltinOps.h"
+
+using namespace mlir;
+
+namespace QRM {
+
+class PassRunner {
+public:
+  // initialize the pass runner
+  void init();
+  void invokePasses(ModuleOp circuit, const std::vector<std::string> &passes);
+  void invokePasses(ModuleOp circuit, const std::vector<std::string> &passes,
+                    std::string device);
+  void applyOptimizationLevel(ModuleOp circuit, const std::string oLevel);
+
+private:
+};
+
+} // namespace QRM
