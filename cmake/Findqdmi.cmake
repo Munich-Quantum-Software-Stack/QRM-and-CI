f311082bf48e170d4c5a8f4781ec5e4d39282513
Martin Letras <di35hec@badwlrz-cl14642>
2025-03-17 10:32:30 +0000

    📝 Updating fetch content of qdmo

--- cmake/Findqdmi.cmake
+++ cmake/Findqdmi.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
   qdmi
   GIT_REPOSITORY git@github.com:Munich-Quantum-Software-Stack/QDMI.git
-  GIT_TAG testing)
+  GIT_TAG develop)
 
 FetchContent_MakeAvailable(qdmi)
