d98f73ce40904e5acbe9b1dfd5d82315326162db
Martin Letras <di35hec@badwlrz-cl14642>
2025-03-28 15:55:26 +0000

    ✨ Struct is according to python imp.

--- include/mqss/QuantumTask.hpp
+++ include/mqss/QuantumTask.hpp
@@ -1,26 +1,30 @@
 #pragma once
 
 #include <string>
-
+#include <vector>
 namespace mqss {
 struct QuantumTask {
   int task_id;
-  int parent_id;
   int n_qbits;
   int n_shots;
-  std::string circuit_file;
-  std::string circuit_file_type;
+  std::vector<std::string> circuit_files;
+  std::string circuit_file_type; // qasm, quake or qir
   std::string result_destination;
   std::string preferred_qpu;
+  std::string scheduled_qpu;
   // QDMI_Device scheduled_qpu;
   int priority;
-  std::string optimisation_level;
+  int optimisation_level;
   bool no_modify;
   bool transpiler_flag;
   int result_type;
   std::string submit_time;
-  std::string quake;
+  std::vector<std::string> circuits_qiskit; // TODO
   std::string additional_information;
+  std::vector<std::string> restricted_resource_names;
+  std::string user_identity;
+  std::string token;
+  bool via_hpc;
   // ThreadSafeModule thread_safe_module;
 };
 
