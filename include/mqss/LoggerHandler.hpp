7b467276d86c65303a7533661e33594a19b159e4
Martin Letras <di35hec@badwlrz-cl14642>
2025-04-07 12:25:05 +0000

    ✨ Included header with the name of the loggers

---
+++ include/mqss/LoggerHandler.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#define LOGGER_QRM "mqss::QRM"
+#define LOGGER_AGNOSTIC_PASS_RUNNER "mqss::AgnosticPassRunner"
+#define LOGGER_SCHEDULER "mqss::Scheduler"
+#define LOGGER_TRANSPILER "mqss::Transpiler"
+#define LOGGER_SUBMITTER "mqss::Submitter"
+
+#define FILE_LOGGER_QRM "QRM.log"
+#define FILE_LOGGER_AGNOSTIC_PASS_RUNNER "AgnosticPassRunner.log"
+#define FILE_LOGGER_SCHEDULER "Scheduler.log"
+#define FILE_LOGGER_TRANSPILER "Transpiler.log"
+#define FILE_LOGGER_SUBMITTER "Submitter.log"
