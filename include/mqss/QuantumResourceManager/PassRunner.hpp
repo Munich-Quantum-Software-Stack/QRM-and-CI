74d502d35b81e89ae5e21d4de9114e68cb0ce827
Martin Letras <di35hec@badwlrz-cl14642>
2025-04-07 15:16:56 +0000

    ✨ Extending pass runner to receive array of circuits

--- include/mqss/QuantumResourceManager/PassRunner.hpp
+++ include/mqss/QuantumResourceManager/PassRunner.hpp
@@ -16,9 +16,16 @@ public:
   // initialize the pass runner
   void init();
   void invokePasses(ModuleOp circuit, const std::vector<std::string> &passes);
+  void invokePasses(std::vector<ModuleOp> circuits,
+                    const std::vector<std::string> &passes);
   void invokePasses(ModuleOp circuit, const std::vector<std::string> &passes,
                     std::string device);
+  void invokePasses(std::vector<ModuleOp> circuits,
+                    const std::vector<std::string> &passes, std::string device);
   void applyOptimizationLevel(ModuleOp circuit, int oLevel);
+  void applyOptimizationLevel(std::vector<ModuleOp> circuits, int oLevel);
+  void transpile(ModuleOp circuit);
+  void transpile(std::vector<ModuleOp> circuits);
 
 private:
 };
