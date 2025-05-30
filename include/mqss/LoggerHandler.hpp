eefbf3328569732d0377f8340b0d0434740148e9
Martin Letras <di35hec@badwlrz-cl14642>
2025-05-30 11:51:27 +0000

    🚀 Define logger for the mock device

--- include/mqss/LoggerHandler.hpp
+++ include/mqss/LoggerHandler.hpp
@@ -5,9 +5,11 @@
 #define LOGGER_SCHEDULER "mqss::Scheduler"
 #define LOGGER_TRANSPILER "mqss::Transpiler"
 #define LOGGER_SUBMITTER "mqss::Submitter"
+#define LOGGER_MOCK_DEVICE "mqss::MockDevice"
 
 #define FILE_LOGGER_QRM "QRM.log"
 #define FILE_LOGGER_AGNOSTIC_PASS_RUNNER "AgnosticPassRunner.log"
 #define FILE_LOGGER_SCHEDULER "Scheduler.log"
 #define FILE_LOGGER_TRANSPILER "Transpiler.log"
 #define FILE_LOGGER_SUBMITTER "Submitter.log"
+#define FILE_LOGGER_MOCK_DEVICE "Mock-Device.log"
