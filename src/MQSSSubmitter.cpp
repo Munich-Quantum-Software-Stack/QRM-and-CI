a07e87e150d46770de66665a321118ad7e118871
akshay9594 <akshay9407@outlook.com>
2026-06-24 13:32:03 +0000

    [MQSS] Copied over an example QDMI device object file and printing results in test-case

--- src/MQSSSubmitter.cpp
+++ src/MQSSSubmitter.cpp
@@ -31,122 +31,8 @@
 #include <utility>
 #include <vector>
 
-#define QDMI_CONF_PATH "cxx_qdmi.conf"
 
-using namespace mqss;
-struct CouplingTy {
-  uint64_t src_id;
-  uint64_t dst_id;
-};
-
-struct DeviceProperty {
-  int numQubits = -223;
-  std::vector<CouplingTy> cm;
-};
-
-// API to fetch number of Qubits of the target device
-static int getDeviceNumQubits(QDMI_Device device) {
-
-  // Step 1: get the size
-  size_t size_ret = 0;
-  int ret = -223;
-  int numQubits;
-
-  ret = QDMI_device_query_device_property(
-      device, QDMI_DEVICE_PROPERTY_QUBITSNUM,
-      sizeof(size_t), // ← size of the buffer you're providing
-      &numQubits,     // ← pointer to receive the value directly
-      nullptr         // ← size_ret not needed);
-  );
-  assert(ret == QDMI_SUCCESS);
-  return numQubits;
-}
-
-// Fetching the coupling map of the target device
-static std::vector<CouplingTy>
-getDeviceCouplingMap(QDMI_Device device,
-                     std::shared_ptr<spdlog::logger> MQSSLogger) {
-  // Step 1: get the size
-  size_t size_ret = 0;
-  int ret = -223;
-  ret = QDMI_device_query_device_property(
-      device, QDMI_DEVICE_PROPERTY_COUPLINGMAP, 0, nullptr, &size_ret);
-
-  MQSSLogger->info("-->Query coupling map size: ", size_ret);
-  assert(ret == QDMI_SUCCESS);
-  // size_ret = 20 * sizeof(QDMI_Site) for the cxx device
-  size_t num_entries = size_ret / sizeof(QDMI_Site); // = 20
-  size_t num_pairs = num_entries / 2;                // = 10
-
-  // Step 2: retrieve
-  std::vector<QDMI_Site> queired_coupling_map(num_entries);
-  ret = QDMI_device_query_device_property(
-      device, QDMI_DEVICE_PROPERTY_COUPLINGMAP, size_ret,
-      static_cast<void *>(queired_coupling_map.data()), nullptr);
-
-  MQSSLogger->info("-->Query coupling map entries: ",
-                   queired_coupling_map.size());
-  assert(ret == QDMI_SUCCESS);
-  std::vector<CouplingTy> coupling_map_set;
-  // Step 3: iterate over pairs
-  for (size_t i = 0; i < num_entries; i += 2) {
-    QDMI_Site src = queired_coupling_map[i];
-    QDMI_Site dst = queired_coupling_map[i + 1];
-
-    // query the index of each site
-    uint64_t src_id = 0, dst_id = 0;
-    QDMI_device_query_site_property(device, src, QDMI_SITE_PROPERTY_INDEX,
-                                    sizeof(uint64_t), &src_id, nullptr);
-    QDMI_device_query_site_property(device, dst, QDMI_SITE_PROPERTY_INDEX,
-                                    sizeof(uint64_t), &dst_id, nullptr);
-
-    CouplingTy coupling;
-    coupling.src_id = src_id;
-    coupling.dst_id = dst_id;
-    coupling_map_set.push_back(coupling);
-  }
-  return coupling_map_set;
-}
-
-// Getting device properties such as No. of Qubits, Coupling Map etc.
-// TODO: Need to enable this.
-static DeviceProperty
-getDeviceProperties(QDMI_Device Device,
-                    std::shared_ptr<spdlog::logger> MQSSLogger) {
-
-  DeviceProperty Properties;
-  Properties.numQubits = getDeviceNumQubits(Device);
-  Properties.cm = getDeviceCouplingMap(Device, std::move(MQSSLogger));
-
-  return Properties;
-}
-
-// Test to check the complex state vector amplitudes received after executing
-// the QDMI Job.
-// TODO: Need a better check here!
-::testing::AssertionResult
-CheckBellState(const std::vector<std::complex<double>> &state_vector,
-               std::shared_ptr<spdlog::logger> MQSSLogger, double tol = 1e-6) {
-  if (!state_vector.empty() && state_vector.size() != 32) {
-    MQSSLogger->error("Expected 32 amplitudes got: {}", state_vector.size());
-    return testing::AssertionFailure();
-  }
-  constexpr double inv_sqrt2 = 0.70710678118654752440;
-  const double a00 = std::abs(state_vector[0]);
-  const double a01 = std::abs(state_vector[1]);
-  const double a10 = std::abs(state_vector[2]);
-  const double a11 = std::abs(state_vector[3]);
-
-  MQSSLogger->info(
-      "Results of Quantum Job are the following (First 4 amplitudes):");
-  MQSSLogger->info("|00> amplitude: {}", a00);
-  MQSSLogger->info("|01> amplitude: {}", a01);
-  MQSSLogger->info("|10> amplitude: {}", a10);
-  MQSSLogger->info("|11> amplitude: {}", a11);
-
-  return ::testing::AssertionSuccess();
-}
-
+// Load the device library dynamically
 static std::pair<std::reference_wrapper<qdmi_main_driver::Driver>, QDMI_Device>
 addDynamicDeviceLibrary(const std::string &libName, const std::string &prefix,
                         std::shared_ptr<spdlog::logger> MQSSLogger) {
@@ -184,7 +70,7 @@ addDynamicDeviceLibrary(const std::string &libName, const std::string &prefix,
 }
 
 // Use QDMI API's to create a QDMI Job and submit it to the QDMI device.
-static QuantumResult createAndSubmitQDMIJobToQDMIDevice(
+static mqss::QuantumResult createAndSubmitQDMIJobToQDMIDevice(
     mqss::QuantumTask task, const std::string &libName,
     const std::string &prefix, std::shared_ptr<spdlog::logger> MQSSLogger) {
 
@@ -256,7 +142,6 @@ static QuantumResult createAndSubmitQDMIJobToQDMIDevice(
   // const size_t vec_length = state_size / sizeof(double);
   // assert(vec_length % 2 == 0);
 
-  MQSSLogger->info("Raw HIST KEYS size: {}", size);
   std::string key_list(size - 1, '\0');
   ret = QDMI_job_get_results(
       job, QDMI_JOB_RESULT_HIST_KEYS, size,
@@ -283,19 +168,21 @@ static QuantumResult createAndSubmitQDMIJobToQDMIDevice(
                              static_cast<void *>(counts.data()), nullptr);
   assert(ret == QDMI_SUCCESS);
 
-  for (size_t i = 0; i < counts.size(); i++) {
-    MQSSLogger->info("counts[{}]: {}", i, counts[i]);
-  }
-
-  QuantumResult result;
+  mqss::QuantumResult result;
   result.set_task_id(task.task_id());
 
   result.add_executed_circuits(circuit);
-  std::string return_String = "";
-  for (auto entry : key_vec) {
-    return_String += entry + " ";
+
+  result.set_execution_status(0);
+
+  auto *res = result.add_results();
+  if (key_vec.size() != counts.size()) {
+    MQSSLogger->error("Size of Keys Vector not equal to Number of Counts!");
+  };
+
+  for (unsigned i = 0; i < key_vec.size(); ++i) {
+    (*res->mutable_counts())[key_vec[i]] = counts[i];
   }
-  result.set_additional_information(return_String);
 
   QDMI_job_free(job);
   // QDMI_session_free(session);
@@ -312,12 +199,12 @@ int main() {
   });
 
   std::signal(SIGTERM, [](int) {
-    Logger::cleanup();
+    mqss::Logger::cleanup();
     exit(0);
   });
 
   mqss::Logger::init(SUBMITTER_LOG_FILES_PATH, LOGGER_SUBMITTER);
-  auto logger = Logger::getLogger();
+  auto logger = mqss::Logger::getLogger();
   logger->info("Running up the MQSS Submitter");
 
   mqss::TransportOptions<mqss::RabbitMqSimple> opts;
@@ -358,12 +245,15 @@ int main() {
     // }
 
     // prepare QDMI job and submit
-    std::string libName = "/workspaces/QRM/_deps/core/build/src/qdmi/devices/"
-                          "dd/libmqt-core-qdmi-ddsim-device.so";
+    // Set the path to the QDMI Device Shared Object file
+    std::string libName = "/workspaces/QRM/qdmi_device_objs/"
+                          "libmqt-core-qdmi-ddsim-device.so.3.3.4";
+
+    std::string device_prefix = "MQT_DDSIM";
     logger->info("Device conf is: " + libName);
 
-    auto circuit_result =
-        createAndSubmitQDMIJobToQDMIDevice(task, libName, "MQT_DDSIM", logger);
+    auto circuit_result = createAndSubmitQDMIJobToQDMIDevice(
+        task, libName, device_prefix, logger);
 
     // auto circuit_result =
     //     createAndSubmitQDMIJob(task, device_conf.c_str(), std::move(logger));
